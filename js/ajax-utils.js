7fd8c7f67c4377ea0c514369290c0be78d991b0d
Parvez Hossain <pvzhossain@gmail.com>
2022-08-25 23:56:22 +0600

    update_v2

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -3,7 +3,6 @@
 // Set up a namespace for our utility
 var ajaxUtils = {};
 
-
 // Returns an HTTP request object
 function getRequestObject() {
   if (window.XMLHttpRequest) {
@@ -19,28 +18,19 @@ function getRequestObject() {
   }
 }
 
-
 // Makes an Ajax GET request to 'requestUrl'
 ajaxUtils.sendGetRequest = 
   function(requestUrl, responseHandler, isJsonResponse) {
     var request = getRequestObject();
     request.onreadystatechange = 
       function() { 
-        handleResponse(request, 
-                       responseHandler,
-                       isJsonResponse); 
+        handleResponse(request, responseHandler,isJsonResponse); 
       };
     request.open("GET", requestUrl, true);
     request.send(null); // for POST only
   };
 
-
-// Only calls user provided 'responseHandler'
-// function if response is ready
-// and not an error
-function handleResponse(request,
-                        responseHandler,
-                        isJsonResponse) {
+function handleResponse(request,responseHandler,isJsonResponse) {
   if ((request.readyState == 4) &&
      (request.status == 200)) {
 
@@ -58,8 +48,6 @@ function handleResponse(request,
   }
 }
 
-
-// Expose utility to the global object
 global.$ajaxUtils = ajaxUtils;
 
 
